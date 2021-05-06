61000e236a48d8a1216009b09c91685b567a7859
Norman Cerros <n_cerros@hotmail.com>
2021-05-06 11:13:01 -0600

    modified api/categoriy and product-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,7 +36,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
